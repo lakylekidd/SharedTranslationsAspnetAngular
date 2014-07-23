4c10b5bb6b870d9e56dae5804bc402c452ad2a48
Jerrie Pelser <jerrie@jerriepelser.com>
2014-07-23 10:48:14 +0700

    Initial checkin

---
+++ SharedTranslationsAspnetAngular/Controllers/TranslationsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json.Linq;
+
+namespace SharedTranslationsAspnetAngular.Controllers
+{
+    public class TranslationsController : ApiController
+    {
+        public IHttpActionResult Get(string lang)
+        {
+            var resourceObject = new JObject();
+
+            var resourceSet = Resources.Resources.ResourceManager.GetResourceSet(new CultureInfo(lang), true, true);
+            IDictionaryEnumerator enumerator = resourceSet.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                resourceObject.Add(enumerator.Key.ToString(), enumerator.Value.ToString());
+            }
+
+            return Ok(resourceObject);
+        }
+    }
+}
